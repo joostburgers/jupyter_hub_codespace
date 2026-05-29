554c8e63068fbfbe97bfced61f83933de9a11f01
Joost Burgers <joost@jmu.edu>
2026-05-29 09:22:10 -0400

    chore: move publish/convert scripts to instructor/ (local-only, hidden from students)

--- analyze_ch.py
+++
@@ -1,9 +0,0 @@
-﻿import json, re
-with open('project_mapping_emotions/interactive_tour_config.js', 'r', encoding='utf-8') as f:
-    c = f.read()
-j = re.sub(r'^.*?=\s*', '', c, count=1, flags=re.DOTALL).rstrip().rstrip(';')
-d = json.loads(j)
-ch0 = d['chapters'][0]
-print('showData:', ch0.get('showData'))
-print('id:', ch0.get('id'))
-print('location:', ch0.get('location'))
